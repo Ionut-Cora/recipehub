f6dd7c5afdc7dddb4ed2684f4551622b37c1ecc0
Ionut Cora <cora.ionut92@yahoo.com>
2026-07-31 23:07:05 +0100

    Display the recent published recipes on homepage

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+from .models import Recipe
+
 
 def home(request):
     """
-    Display the RecipeHub Homepage.
+    Display the homepage with the newest published recipes.
     """
-    return render(request, "recipes/home.html")
+
+    recent_recipes = (
+        Recipe.objects
+        .filter(status=Recipe.Status.PUBLISHED)
+        .select_related("author", "category")
+        .order_by("-created_on")[:3]
+    )
+
+    context = {
+        "recent_recipes": recent_recipes,
+    }
+
+    return render(request, "recipes/home.html", context)
