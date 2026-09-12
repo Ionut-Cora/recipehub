a7635a47c63d539ee82193cc9317c9b9fc7a1400
Ionut Cora <cora.ionut92@yahoo.com>
2026-09-12 23:16:45 +0100

    Add the secret key to the project

--- recipehub/settings.py
+++ recipehub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bxh-pk6&jcjb1!m0p&k#wd^0ewlfg@y6f#tjll0j1-&i9+j=ac'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
